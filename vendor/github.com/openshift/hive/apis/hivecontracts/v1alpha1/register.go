b9e81e1bd3c77f89e7b875db76f1c7b6e6bfdb96
Mark Old <mold@redhat.com>
2024-06-28 12:06:59 -0700

    Modernize update-codegen.sh

--- vendor/github.com/openshift/hive/apis/hivecontracts/v1alpha1/register.go
+++ vendor/github.com/openshift/hive/apis/hivecontracts/v1alpha1/register.go
@@ -3,8 +3,7 @@
 // Package v1alpha1 contains API Schema definitions for the hivecontracts v1alpha1 API group
 // +k8s:openapi-gen=true
 // +k8s:deepcopy-gen=package,register
-// +k8s:conversion-gen=github.com/openshift/hive/apis/hivecontracts
-// +k8s:defaulter-gen=TypeMeta
+// +k8s:conversion-gen=false
 // +groupName=hivecontracts.openshift.io
 package v1alpha1
 
